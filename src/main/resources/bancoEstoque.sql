26d113d443608b0f099fa089411bb3586667310c
Nicolas Peruch <nicolas.peruch@ilegra.com>
2017-05-12 14:19:53 -0300

    Correcao do script do bd.

--- src/main/resources/bancoEstoque.sql
+++ src/main/resources/bancoEstoque.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `item`
 --
 
+CREATE DATABASE `mydb`;
+USE `mydb`;
+
 DROP TABLE IF EXISTS `item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
